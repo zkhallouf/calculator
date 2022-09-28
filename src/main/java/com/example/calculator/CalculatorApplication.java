1486d3db9b3061246eb94e27302446ad3809f5bb
zkhallouf <zainab.khallouf@gmail.com>
2022-09-28 13:13:49 +0000

    Add sum logic, controller and unit test

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -1,13 +1,18 @@
 package com.example.calculator;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example"})
 public class CalculatorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
 }
+
+
+
+
+
